5892a0eea1bc16ca2d3517856a1faa9de5150634
Jack-Z112233 <jackzqy1225@gmail.com>
2025-10-02 15:11:25 -0400

    Add PositiveInteger

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                int[] tests = {1, 6, 10};
+                for (int i = 0; i < tests.length; i++) {   // classic index loop
+                    int x = tests[i];
+                    PositiveInteger p = new PositiveInteger(x);
+                    System.out.println(x + " perfect? " + p.isPerfect());
+
+                }
+            }
+        }
---
+++ src/PositiveInteger.java
@@ -0,0 +1,29 @@
+public class PositiveInteger {
+
+    private int num;
+
+    public PositiveInteger(int number) {
+        num = number;
+    }
+
+    public boolean isPerfect() {
+        if (num == 1) return false;
+        int x = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) x += i;
+        }
+        return x == num;
+    }
+
+
+    public boolean isAbundant() {
+
+        return false;
+
+    }
+
+    public boolean isNarcissistic() {
+
+        return false;
+    }
+}
