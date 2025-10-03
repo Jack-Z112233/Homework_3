08e244c38ce53497bbd9322ffc92679eb00fa4f2
Jack-Z112233 <jackzqy1225@gmail.com>
2025-10-02 21:39:16 -0400

    Finished Q2 Needle

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-                int[] tests = {1, 6, 10, 12, 14, 15, 153};
-                for (int i = 0; i < tests.length; i++) {   // classic index loop
-                    int x = tests[i];
-                    PositiveInteger p = new PositiveInteger(x);
-                    System.out.println(x + " perfect? " + p.isPerfect());
-                    System.out.println(x + " Abundant? " + p.isAbundant());
-                    System.out.println(x + " Narcissistic? " + p.isNarcissistic());
-                }
-            }
+        int[] tests = {1, 6, 10, 12, 14, 15, 153};
+        for (int i = 0; i < tests.length; i++) {   // classic index loop
+            int x = tests[i];
+            PositiveInteger p = new PositiveInteger(x);
+            System.out.println(x + " perfect? " + p.isPerfect());
+            System.out.println(x + " Abundant? " + p.isAbundant());
+            System.out.println(x + " Narcissistic? " + p.isNarcissistic());
         }
+        //needle testing
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Drops in total: ");
+        int D = sc.nextInt(); //input number of drops
+        double pi = new Needle().runExperiment(D);
+        System.out.println("Pi is: " + pi);
+
+    }
+}
---
+++ src/Needle.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Needle {
+
+    private Random generator;
+    public Needle() {
+        generator = new Random();
+    }
+
+    public double runExperiment(int totalDrops) {
+        int hits = 0;
+        for (int i = 0; i < totalDrops; i++) {
+            double y_low = generator.nextDouble() * 2.0; //generates a double from 0 to 1.0 *2
+            double alpha = generator.nextDouble() * 180.0; //random degree from 0 to 180
+            double alphaRadian = Math.toRadians(alpha); // convert degree to radian
+            double y_high = y_low + Math.sin(alphaRadian); //the top point of the needle
+            if (y_high >= 2.0) {
+                hits++; //if it is over 2.0 inches, it hits so plus one on total hit number
+            }
+        }
+        if (hits == 0) {
+            System.out.println("Experiment failed.");
+        } //just in case that the super rare possibility that this happens
+        return (double) totalDrops / hits; //calculates pi
+    }
+}
